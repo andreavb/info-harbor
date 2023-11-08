c61a78d00bdc555880472d45f607bbe9aa5bf55d
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2023-11-08 13:39:24 -0300

    lint: remove unused imports and sort remaining

--- chess_results.py
+++ chess_results.py
@@ -1,15 +1,10 @@
-import os
-import re
 import concurrent.futures
-
-import chess.pgn
+import os
 
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
 
